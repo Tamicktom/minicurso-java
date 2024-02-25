9de707652b455496aa0f6198031641eb0b22f1dc
Tamicktom <rickpokemon38@gmail.com>
2024-02-25 02:55:11 -0300

    saving

--- src/main/java/app/hammertail/todolist/user/UserController.java
+++ src/main/java/app/hammertail/todolist/user/UserController.java
@@ -1,10 +1,14 @@
 package app.hammertail.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -14,12 +18,18 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity<UserModel> create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
     if (user != null) {
+      ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
       throw new RuntimeException("Username already exists");
     }
+
+    var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashed);
+
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
